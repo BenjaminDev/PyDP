d1cb378d580eee0c4b744ed3df04ccadc91e331d
benjamindev <benjamin.decharmoy@gmail.com>
2020-08-13 13:12:20 +0000

    added order statistics in a similar way to the bounded functions. Making use of Algorithm base class.

---
+++ pydp/algorithms/laplacian/order_statistics.py
@@ -0,0 +1,17 @@
+from ..algorithm import Algorithm
+
+
+class Max(Algorithm):
+    pass
+
+
+class Min(Algorithm):
+    pass
+
+
+class Median(Algorithm):
+    pass
+
+
+class Percentile(Algorithm):
+    pass
